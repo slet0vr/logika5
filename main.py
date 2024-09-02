0bb3b6425550e8a3173464fcb6c3f659e670a8d4
slet0ver <egorpetrovskij444@gmail.com>
2024-09-02 16:18:38 +0300

    copy from task

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+class Widget():
+    def __init__(self, x, y, label):
+        self.x = x
+        self.y = y
+        self.label = label
+
+    def print(self):
+        print("Напис:", self.label)
+        print("Розташування:", self.x, self.y)
+
+class Button(Widget):
+    def __init__(self, x, y, label, is_clicked):
+        super().__init__(x, y, label)
+        self.is_clicked = is_clicked
+
+    def click(self):
+        self.is_clicked = True
+        print("Ви зареєстровані")
+
+btn = Button(100, 100, "Брати участь", False)
 
